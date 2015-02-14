dd7bb42d48fe0e2c9e86484a737ce9eac0fef62b
David Carvalho <david.carvalho@ocado.com>
2015-02-14 10:41:01 +0000

    Now the addMappings actually adds new mappings instead of just overriding the previous ones.

---
+++ CTXPropertyMapperTests/User.h
@@ -0,0 +1,17 @@
+//
+//  User.h
+//  OOGUiCommon
+//
+//  Created by David Carvalho on 13/02/2015.
+//  Copyright (c) 2015 Ocado Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+@property (nonatomic, strong) NSString *firstNameSomething;
+@property (nonatomic, strong) NSString *lastName;
+@property (nonatomic, assign) BOOL active;
+
+@end
---
+++ CTXPropertyMapperTests/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  OOGUiCommon
+//
+//  Created by David Carvalho on 13/02/2015.
+//  Copyright (c) 2015 Ocado Ltd. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
